311658d71aeb4629c164a0aef1a730512a8adeaf
James Noleto Brandao da Silva <muricijames@gmail.com>
2021-10-05 16:48:23 -0300

    Algoritmo de ordenação SelectionSort

---
+++ ORDENACAO/SelectionSort.js
@@ -0,0 +1,33 @@
+//Selection sort: busca o menor valor e adiciona-o no começo do vetor;
+
+function SelectionSort(){
+
+    var vetor = [13, 9, 5, 3];
+
+    var auxiliar = 0;
+
+    for(var i = 0; i < vetor.length - 1; i++){
+
+        var menor = i;
+
+        for(var j = i + 1; j < vetor.length; j++){
+
+            if(vetor[menor] > vetor[j]){
+                menor = j;
+            }
+
+        }
+
+        if(menor != i){
+            auxiliar = vetor[i];
+            vetor[i] = vetor[menor];
+            vetor[menor] = auxiliar;
+        }
+
+    }
+
+    return vetor;
+
+}
+
+console.log(SelectionSort());
